a251f9c2c5d5d88db9f393ac2c69bb3d5daa6624
unknown <Andrew M>
2022-12-11 16:50:23 +0300

    The 1-st tak is done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿// написать программу, которая принимает на вход координаты 
+// точки (х, у), причем они не равны 0,
+// программа должна выдавать четверти плоскости, в которой 
+// находятся эти точки
+
+void SearchSquad(int x, int y){
+    if(x==0 || y==0)
+        Console.WriteLine("Error");
+    else if(x>0 && y<0)
+        Console.WriteLine("Point is in the 4th Squad");
+    else if(x<0 && y<0)
+        Console.WriteLine("Point is in the 3rd Squad");
+    else if(x<0 && y>0)
+        Console.WriteLine("Point is in the 2nd Squad");
+    else
+        Console.WriteLine("Point is in the 1st Squad");
+}
+
+Console.Write("Enter coordinate of X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter coordinate of Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+SearchSquad(x, y);
